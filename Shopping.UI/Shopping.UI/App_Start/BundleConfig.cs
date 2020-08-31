0348748acc95e4e9394721405ea650fe497ae544
Nawinav <avnaveenkumar16@gmail.com>
2020-08-31 12:47:52 +0530

    view

--- Shopping.UI/Shopping.UI/App_Start/BundleConfig.cs
+++ Shopping.UI/Shopping.UI/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Shopping.UI
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Lumen.css",
                       "~/Content/site.css"));
         }
     }
