f1a8e6803f509a4ec9feef6dd43ee913c375009f
Nawinav <avnaveenkumar16@gmail.com>
2020-08-31 17:09:26 +0530

    Add Product Image to the project

--- Shopping.UI/Shopping.UI/Controllers/ProductManagerController.cs
+++ Shopping.UI/Shopping.UI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,7 +37,7 @@ namespace Shopping.UI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -44,12 +45,17 @@ namespace Shopping.UI.Controllers
             }
             else
             {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//Productimages//")+product.Image);
+                }
                 context.Insert(product);
                 context.commit();
             }
             return RedirectToAction("Index");
         }
-        public ActionResult Edit(string Id)
+        public ActionResult Edit(string Id, HttpPostedFileBase file)
         {
             Product product = context.Find(Id);
             ProductCategory category = new ProductCategory();
@@ -67,7 +73,7 @@ namespace Shopping.UI.Controllers
             }
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -81,11 +87,15 @@ namespace Shopping.UI.Controllers
                 {
                     return View(product);
                 }
+                if (file != null)
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//Productimages//")+ productToEdit.Image);
+                }
                 productToEdit.Name = product.Name;
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
                 productToEdit.Price = product.Price;
-                productToEdit.Image = product.Image;
                 context.commit();
                 return RedirectToAction("Index");
             }
