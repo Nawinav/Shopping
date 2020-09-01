21a4ca66428b5e9d39e498c19608f3616753e83a
Nawinav <avnaveenkumar16@gmail.com>
2020-09-01 13:09:21 +0530

    Added BasketModel And Updated the database

--- Shopping.UI/Shopping.SQL/DataContext.cs
+++ Shopping.UI/Shopping.SQL/DataContext.cs
@@ -17,5 +17,7 @@ namespace Shopping.SQL
         }
         public DbSet<Product> products { get; set; }
         public DbSet<ProductCategory> productCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
